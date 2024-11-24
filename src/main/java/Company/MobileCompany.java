419933059f60245a43b7285038f9dc20d08b07e6
FiremanC4 <firemanplay66@gmail.com>
2024-11-24 21:28:27 +0200

    added delete tariff func

--- src/main/java/Company/MobileCompany.java
+++ src/main/java/Company/MobileCompany.java
@@ -5,4 +5,28 @@ import java.util.ArrayList;
 public class MobileCompany {
     ArrayList<Tariff> tariffs;
 
+    public MobileCompany(ArrayList<Tariff> tariffs) {
+        this.tariffs = tariffs;
+    }
+
+    public MobileCompany() {
+        this.tariffs = new ArrayList<Tariff>();
+    }
+
+    boolean deleteTariff(String name) {
+        int idx = -1;
+        for (int i = 0; i < tariffs.size(); i++) {
+            if (tariffs.get(i).getName().equals(name)) {
+                idx = i;
+                break;
+            }
+        }
+        if (idx == -1) {
+            return false;
+        }
+        tariffs.remove(idx);
+        return true;
+    }
+
+
 }
